8a7d7ac35e6a170c1d54410bf511ac3e6c162eb6
William <williaj@stud.ntnu.no>
2019-10-22 20:08:48 +0200

    dir change

--- Exercices/Exercrise 8/drawIllusion.py
+++ Exercices/Exercrise 8/drawIllusion.py
@@ -1,1 +1,32 @@
-print("Hello world")
+import turtle
+
+
+def drawGrid(colorGrid, sizeSquare, sizeImage):
+    turtle.setup(sizeImage, sizeImage)
+    print("drawingRectangle")
+    bob = turtle.Turtle()
+    bob.color(colorGrid)
+    bob.pensize(5)
+    bob.penup()
+    bob.setx(-sizeImage/2)
+    bob.sety(sizeImage/2)
+    while bob.position < sizeImage:
+        bob.pendown()
+        bob.fd(sizeImage)
+
+
+def drawIllusion(colorGrid, colorSquare, colorCircles, sizeImage, sizeSquare, sizeCircle):
+
+
+    drawGrid(colorGrid, sizeSquare, sizeImage)
+
+    turtle.done()
+
+
+
+def main():
+    drawIllusion("red",2,3,400,5,6)
+
+
+if __name__ == '__main__':
+    main()
